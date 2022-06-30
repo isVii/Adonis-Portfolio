786bff2262f8db2559db86efacf5b6c0e6549859
Vii <40670392+isVii@users.noreply.github.com>
2022-06-30 14:22:31 +0200

    ⭐️ update views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import '../css/app.css'
+
+const email = document.querySelector('.email')
+const notif = document.querySelector('.notif')
+email.addEventListener('click', async () => {
+   await navigator.clipboard.writeText(email.innerHTML)
+    alert('Email copiée')
+})
